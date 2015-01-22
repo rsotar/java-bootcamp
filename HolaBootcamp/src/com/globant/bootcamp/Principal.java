16942254fe7291f9f2af456ba1fb4491df784cad
Roberto Sotar <rsotar@gmail.com>
2015-01-22 00:46:17 -0300

    firs commit

---
+++ HolaBootcamp/src/com/globant/bootcamp/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.globant.bootcamp;
+
+/**
+ * @author rober
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
